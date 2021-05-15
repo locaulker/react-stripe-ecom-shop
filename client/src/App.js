44a770050af88b2b05e824528df278131ebc4133
Lawrence O. Caulker <locaulker@gmail.com>
2021-05-15 11:03:34 -0400

    setup Stripe

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,33 @@ import { QueryClientProvider, QueryClient } from "react-query"
 import Home from "./pages/Home"
 import Product from "./pages/Product"
 import Result from "./pages/Result"
+import { loadStripe } from "@stripe/stripe-js"
+import { CartProvider } from "use-shopping-cart"
 
 const queryClient = new QueryClient()
+
+// prefix with 'REACT_APP_' (when using .env)
+
+const stripePromise = loadStripe(
+  "pk_test_51IrONUDqO0MmNLNvbV7UXtZr1pNhOaxj6uANuDg1Pnxk7zis3Rb1X3dFB1ADM1mxe0RrcmX7xMqGVWwzpaxMeCkF00Res8rch0"
+)
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/result" component={Result} />
-          <Route path="/:productId" component={Product} />
-        </Switch>
-      </BrowserRouter>
+      <CartProvider
+        mode="checkout-session"
+        stripe={stripePromise}
+        currency="USD"
+      >
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/result" component={Result} />
+            <Route path="/:productId" component={Product} />
+          </Switch>
+        </BrowserRouter>
+      </CartProvider>
     </QueryClientProvider>
   )
 }
