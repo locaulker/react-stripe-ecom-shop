88577bd34b1bd82a5e7cf50ad03e96452cd1c481
Lawrence O. Caulker <locaulker@gmail.com>
2021-05-15 02:42:48 -0400

    added product detail endpoint

--- server/routes.js
+++ server/routes.js
@@ -4,9 +4,20 @@ const products = require("./products.json")
 module.exports = function getRoutes() {
   const router = express.Router()
 
-  router.get("/products", (req, res) => {
-    res.status(200).json({ products })
-  })
+  router.get("/products", getProducts)
+  router.get("/products/:productId", getProduct)
 
   return router
+}
+
+// Controllers
+function getProducts(req, res) {
+  res.status(200).json({ products })
+}
+
+function getProduct(req, res) {
+  const { productId } = req.params
+
+  const product = products.find(product => product.id === productId)
+  res.status(200).json({ product })
 }
