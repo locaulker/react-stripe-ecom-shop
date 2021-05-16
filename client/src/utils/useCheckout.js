5b431b7107bf6a530b7c08c6750b7811e46c27a4
Lawrence O. Caulker <locaulker@gmail.com>
2021-05-16 02:25:15 -0400

    created and implemented custom useCheckout

--- client/src/utils/useCheckout.js
+++ client/src/utils/useCheckout.js
@@ -1,1 +1,23 @@
-export default function useCheckout() {}
+import axios from "axios"
+import toast from "react-hot-toast"
+import { useShoppingCart } from "use-shopping-cart"
+
+export default function useCheckout() {
+  const { redirectToCheckout, cartDetails } = useShoppingCart()
+
+  const handleCheckout = async () => {
+    const session = await axios
+      .post("/api/checkout-Sessions", cartDetails)
+      .then(res => res.data)
+      .catch(error => {
+        toast.error("Checkout Failed!")
+        console.log("Error during checkout: ", error)
+      })
+
+    if (session) {
+      redirectToCheckout({ sessionId: session.id })
+    }
+  }
+
+  return handleCheckout
+}
--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,8 @@ module.exports = function getRoutes() {
   router.get("/products", getProducts)
   router.get("/products/:productId", getProduct)
 
+  router.post("/checkout-sessions", createCheckoutSession)
+
   return router
 }
 
@@ -30,3 +32,5 @@ function getProduct(req, res) {
     return res.status(404).json({ statusCode: 404, message: error.message })
   }
 }
+
+function createCheckoutSession() {}
