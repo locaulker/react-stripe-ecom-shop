e1ced5964afd2099d760603d6eef8dd7e907c27b
Lawrence O. Caulker <locaulker@gmail.com>
2021-05-16 00:45:57 -0400

    display all cart items in modal

--- client/src/components/CartModal.js
+++ client/src/components/CartModal.js
@@ -2,11 +2,14 @@
 import React from "react"
 import Modal from "react-modal"
 import { useShoppingCart } from "use-shopping-cart"
+import CartItem from "./CartItem"
 
 Modal.setAppElement("#root")
 
 export default function CartModal({ isOpen, toggleModal }) {
-  const { cartCount, formattedTotalPrice } = useShoppingCart()
+  const { cartCount, formattedTotalPrice, cartDetails } = useShoppingCart()
+
+  const cartItems = Object.keys(cartDetails).map(key => cartDetails[key])
 
   return (
     <Modal
@@ -24,6 +27,9 @@ export default function CartModal({ isOpen, toggleModal }) {
               </div>
             </div>
             <hr />
+            {cartItems.map(cartItem => (
+              <CartItem key={cartItem.id} cartItem={cartItem} />
+            ))}
             <div className="ml-auto mt-4">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
